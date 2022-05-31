88ec9e7dadc4c7cfea987e494231d21123832368
Nikita Galaiko <nikita@galaiko.rocks>
2022-05-31 14:47:50 +0200

    umami: update domainname

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,13 +2,22 @@
 	import '../app.css';
 	import favIcon from '$lib/assets/CodeballIcon-128.ico';
 	import ogImage from '$lib/assets/github_bg.png';
+	import { dev } from '$app/env';
 </script>
 
 <svelte:head>
 	<title>Codeball -> AI-powered code review</title>
 	<meta name="description" content="AI-powered code review" />
-	<meta name="og:image" content="{ogImage}" />
+	<meta name="og:image" content={ogImage} />
 	<link rel="icon" type="image/svg" href={favIcon} />
+
+	{#if !dev}
+		<script
+			async
+			defer
+			data-website-id="a9b93947-2a07-4d7c-bd02-ba2dce8d446a"
+			src="https://umami.codeball.ai/umami.js"></script>
+	{/if}
 </svelte:head>
 
 <main class="min-h-screen bg-stone-100">
