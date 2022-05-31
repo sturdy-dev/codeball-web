01ce4469bb65f80a8e9848a070e34b4daee5f670
Nikita Galaiko <nikita@galaiko.rocks>
2022-05-31 14:47:27 +0200

    add alt to logos

--- src/lib/components/index/Logos.svelte
+++ src/lib/components/index/Logos.svelte
@@ -3,15 +3,19 @@
 	import SturdyLogo from '$lib/assets/logo-sturdy.svg';
 	import OryLogo from '$lib/assets/logo-ory.png';
 
-	const logos = [SturdyLogo, AthenianLogo, OryLogo];
+	const logos = [
+		{ src: SturdyLogo, alt: 'Sturdy' },
+		{ src: AthenianLogo, alt: 'Athenian' },
+		{ src: OryLogo, alt: 'Ory' }
+	];
 </script>
 
 <div class="flex flex-col gap-2">
 	<span class="text-md text-gray-500">Used by</span>
 
 	<div class="flex gap-8 grayscale">
-		{#each logos as logo}
-			<img class="h-6" src={logo} />
+		{#each logos as { src, alt }}
+			<img class="h-6" {src} {alt} />
 		{/each}
 	</div>
 </div>
