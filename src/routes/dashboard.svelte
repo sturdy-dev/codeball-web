a014c99bf0c4a4ba8c5a5d86c20f2af2469ed3e6
Gustav Westling <gustav@getsturdy.com>
2022-06-07 18:18:01 +0200

    Dashboard listing jobs

---
+++ src/routes/dashboard.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { type ContributionJob, list } from '$lib/jobs';
+	import { onMount } from 'svelte';
+	import { Jobs } from '$lib/components/dashboard';
+
+	let jobs: ContributionJob[] = [];
+
+	const load = () => {
+		list().then((data) => {
+			jobs = data;
+		});
+	};
+	onMount(load);
+</script>
+
+<div class="flex flex-col space-y-16 font-mono">
+	<div>
+		<div class="inline-flex min-w-[12rem] flex-col space-y-2 bg-gray-400 p-4">
+			<span class="text-center font-bold uppercase">Time saved</span>
+			<div class="space-x-1 text-center">
+				<span class="text-6xl text-white">52</span><span>h</span>
+			</div>
+		</div>
+	</div>
+
+	{#if jobs.length > 0}
+		<Jobs {jobs} />
+	{/if}
+</div>
