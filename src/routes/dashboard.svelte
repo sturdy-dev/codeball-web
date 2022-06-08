9e45e671923302ecccf1b8799316a53063d07f00
Gustav Westling <gustav@getsturdy.com>
2022-06-08 12:27:14 +0200

    Add stats to the dashboard

--- src/routes/dashboard.svelte
+++ src/routes/dashboard.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { type Job, list } from '$lib/jobs';
 	import { onMount } from 'svelte';
-	import { Jobs } from '$lib/components/dashboard';
+	import { Jobs, Stats } from '$lib/components/dashboard';
 
 	let jobs: Job[] = [];
 
@@ -14,16 +14,8 @@
 </script>
 
 <div class="flex flex-col space-y-16 font-mono">
-	<div>
-		<div class="inline-flex min-w-[12rem] flex-col space-y-2 bg-gray-400 p-4">
-			<span class="text-center font-bold uppercase">Time saved</span>
-			<div class="space-x-1 text-center">
-				<span class="text-6xl text-white">52</span><span>h</span>
-			</div>
-		</div>
-	</div>
-
 	{#if jobs.length > 0}
+		<Stats {jobs} />
 		<Jobs {jobs} />
 	{/if}
 </div>
