e8e0c5600e233effea2289b2559fb623202d881d
Gustav Westling <gustav@getsturdy.com>
2022-06-08 12:27:14 +0200

    Add stats to the dashboard

---
+++ src/lib/components/dashboard/Stats.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+	import { type ContributionJob, type Job } from '$lib/jobs';
+
+	export let jobs: Job[];
+
+	const groupByToMap = <T, Q>(array: T[], predicate: (value: T, index: number, array: T[]) => Q) =>
+		array.reduce((map, value, index, array) => {
+			const key = predicate(value, index, array);
+			map.get(key)?.push(value) ?? map.set(key, [value]);
+			return map;
+		}, new Map<Q, T[]>());
+
+	$: jobsWithPrediction = jobs.filter(
+		(j) => j?.contribution?.predicted_outcome as ContributionJob
+	) as ContributionJob[];
+
+	$: jobsByContributionURL = groupByToMap(
+		jobsWithPrediction,
+		(j: ContributionJob) => j.contribution.url
+	);
+
+	$: statNumberOfContributions = jobsByContributionURL.size;
+	$: statNumberOfJobs = jobsWithPrediction.length;
+	$: statApprovedContributions = 12
+	$: statAvgTimeToMerge = 12
+	$: statMeanTimeToMerge = 12
+	$: statTimeSaved = 12
+</script>
+
+
+<div>
+	<div class="inline-flex min-w-[12rem] flex-col space-y-2 bg-gray-400 p-4">
+		<span class="text-center font-bold uppercase">PRs</span>
+		<div class="space-x-1 text-center">
+			<span class="text-6xl text-white">{statNumberOfContributions}</span><span></span>
+		</div>
+	</div>
+
+	<div class="inline-flex min-w-[12rem] flex-col space-y-2 bg-gray-400 p-4">
+		<span class="text-center font-bold uppercase">Codeball Jobs</span>
+		<div class="space-x-1 text-center">
+			<span class="text-6xl text-white">{statNumberOfJobs}</span><span></span>
+		</div>
+	</div>
+
+	<div class="inline-flex min-w-[12rem] flex-col space-y-2 bg-gray-400 p-4">
+		<span class="text-center font-bold uppercase">Approvals</span>
+		<div class="space-x-1 text-center">
+			<span class="text-6xl text-white">{statApprovedContributions}</span><span></span>
+		</div>
+	</div>
+
+	<div class="inline-flex min-w-[12rem] flex-col space-y-2 bg-gray-400 p-4">
+		<span class="text-center font-bold uppercase">avg Lead Time</span>
+		<div class="space-x-1 text-center">
+			<span class="text-6xl text-white">{statAvgTimeToMerge}</span><span>m</span>
+		</div>
+	</div>
+
+	<div class="inline-flex min-w-[12rem] flex-col space-y-2 bg-gray-400 p-4">
+		<span class="text-center font-bold uppercase">mean Lead Time</span>
+		<div class="space-x-1 text-center">
+			<span class="text-6xl text-white">{statMeanTimeToMerge}</span><span>m</span>
+		</div>
+	</div>
+
+	<div class="inline-flex min-w-[12rem] flex-col space-y-2 bg-gray-400 p-4">
+		<span class="text-center font-bold uppercase">Time Saved</span>
+		<div class="space-x-1 text-center">
+			<span class="text-6xl text-white">{statTimeSaved}</span><span>m</span>
+		</div>
+	</div>
+
+</div>
--- src/lib/components/dashboard/index.ts
+++ src/lib/components/dashboard/index.ts
@@ -1,1 +1,2 @@
 export { default as Jobs } from './Jobs.svelte';
+export { default as Stats } from './Stats.svelte';
