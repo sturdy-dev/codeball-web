4eb3eb4f4fbcb98a6c5d8bbdcbf3ff5da926bbb7
Gustav Westling <gustav@getsturdy.com>
2022-06-08 12:52:39 +0200

    Dashboard loading and error states

--- src/routes/[orgName]/[repoName].svelte
+++ src/routes/[orgName]/[repoName].svelte
@@ -3,20 +3,38 @@
 	import { onMount } from 'svelte';
 	import { Jobs, Stats } from '$lib/components/dashboard';
 	import { page } from '$app/stores';
+	import Spinner from '$lib/Spinner.svelte';
+	import { GitHubLoginButton } from '$lib/components/index';
 
 	let jobs: Job[] = [];
+	let loaded = false;
 
 	const load = () => {
 		listInOrganizationAndRepository($page.params.orgName, $page.params.repoName).then((data) => {
 			jobs = data.jobs;
+			loaded = true;
 		});
 	};
 	onMount(load);
 </script>
 
 <div class="flex flex-col space-y-16 font-mono">
-	{#if jobs.length > 0}
+	{#if !loaded}
+		<div class="flex justify-around">
+			<Spinner />
+		</div>
+	{:else if jobs.length > 0}
 		<Stats {jobs} />
 		<Jobs {jobs} />
+	{:else}
+		<div class="space-y-2 text-gray-600">
+			<p>
+				No jobs found for
+				<span class="text-black">
+					{$page.params.orgName}/{$page.params.repoName}
+				</span> (or access denied).
+			</p>
+			<GitHubLoginButton showText={true} />
+		</div>
 	{/if}
 </div>
--- src/routes/dashboard.svelte
+++ src/routes/dashboard.svelte
@@ -2,20 +2,33 @@
 	import { type Job, list } from '$lib/jobs';
 	import { onMount } from 'svelte';
 	import { Jobs, Stats } from '$lib/components/dashboard';
+	import Spinner from '$lib/Spinner.svelte';
+	import { GitHubLoginButton } from '$lib/components/index';
 
 	let jobs: Job[] = [];
+	let loaded = false;
 
 	const load = () => {
 		list().then((data) => {
 			jobs = data.jobs;
+			loaded = true;
 		});
 	};
 	onMount(load);
 </script>
 
 <div class="flex flex-col space-y-16 font-mono">
-	{#if jobs.length > 0}
+	{#if !loaded}
+		<div class="flex justify-around">
+			<Spinner />
+		</div>
+	{:else if jobs.length > 0}
 		<Stats {jobs} />
 		<Jobs {jobs} />
+	{:else}
+		<div class="space-y-2 text-gray-600">
+			<p>No jobs found (or access denied).</p>
+			<GitHubLoginButton showText={true} />
+		</div>
 	{/if}
 </div>
