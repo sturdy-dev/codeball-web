ef3d4cb37ca8ddd439c64dcc21befc9192c0ad86
Gustav Westling <gustav@getsturdy.com>
2022-06-09 12:26:23 +0200

    Add lead times to the dashboard

--- src/lib/components/dashboard/Stats.svelte
+++ src/lib/components/dashboard/Stats.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { type ContributionJob, type Job } from '$lib/jobs';
+	import Time from './Time.svelte';
 
 	export let jobs: Job[];
 
@@ -10,9 +11,9 @@
 			return map;
 		}, new Map<Q, T[]>());
 
-	$: jobsWithPrediction = jobs.filter(
+	$: jobsWithPrediction = jobs?.filter(
 		(j) => j?.contribution?.predicted_outcome as ContributionJob
-	) as ContributionJob[];
+	) as ContributionJob[] || [];
 
 	$: jobsByContributionURL = groupByToMap(
 		jobsWithPrediction,
@@ -33,59 +34,131 @@
 		return res;
 	};
 
+	const secondsToMerge = (job: ContributionJob) => {
+		const createdAt = new Date(job.contribution.created_at);
+		const mergedAt = new Date(job.contribution.actual_outcome.merged_at);
+		return (mergedAt.getTime() - createdAt.getTime()) / 1000;
+	};
+
+	const median = (arr) => {
+		const mid = Math.floor(arr.length / 2),
+			nums = [...arr].sort((a, b) => a - b);
+		return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+	};
+
 	$: latestJobsForContribution = getLatestByURL(jobsByContributionURL).sort(byPredictedAt);
+
+	$: mergedContributionJobs = latestJobsForContribution.filter(
+		(j) => j.contribution.actual_outcome
+	);
+	$: mergedContributionJobsApprovedByCodeball = mergedContributionJobs.filter(
+		(j) => j.contribution.predicted_outcome.prediction === 'approved'
+	);
+	$: mergedContributionJobsRejectedByCodeball = mergedContributionJobs.filter(
+		(j) => j.contribution.predicted_outcome.prediction !== 'approved'
+	);
+
+	const avgTimeToMergeSeconds = (jobs: ContributionJob[]) => {
+		return jobs.map(secondsToMerge).reduce((a, b) => a + b, 0) / jobs.length;
+	};
+
+	const medianTimeToMergeSeconds = (jobs: ContributionJob[]) => {
+		return median(jobs.map(secondsToMerge));
+	};
+
+	$: timeToMergeAvgSeconds = avgTimeToMergeSeconds(mergedContributionJobs);
+	$: timeToMergeApprovedByCodeballAvgSeconds = avgTimeToMergeSeconds(
+		mergedContributionJobsApprovedByCodeball
+	);
+	$: timeToMergeRejectedByCodeballAvgSeconds = avgTimeToMergeSeconds(
+		mergedContributionJobsRejectedByCodeball
+	);
+
+	$: timeToMergeMedianSeconds = medianTimeToMergeSeconds(mergedContributionJobs);
+	$: timeToMergeApprovedByCodeballMedianSeconds = medianTimeToMergeSeconds(
+		mergedContributionJobsApprovedByCodeball
+	);
+	$: timeToMergeRejectedByCodeballMedianSeconds = medianTimeToMergeSeconds(
+		mergedContributionJobsRejectedByCodeball
+	);
 
 	$: statNumberOfContributions = jobsByContributionURL.size;
 	$: statNumberOfJobs = jobsWithPrediction.length;
 	$: statApprovedContributions = latestJobsForContribution.filter(
 		(j) => j.contribution.predicted_outcome.prediction === 'approved'
 	).length;
-	$: statAvgTimeToMerge = 12;
-	$: statMeanTimeToMerge = 12;
-	$: statTimeSaved = 12;
 </script>
 
-<div>
-	<div class="inline-flex min-w-[12rem] flex-col space-y-2 bg-gray-400 p-4">
+<div class="flex gap-2">
+	<div class="inline-flex min-w-[12rem] grow flex-col space-y-2 bg-gray-400 p-4">
 		<span class="text-center font-bold uppercase">PRs</span>
 		<div class="space-x-1 text-center">
 			<span class="text-6xl text-white">{statNumberOfContributions}</span><span />
 		</div>
 	</div>
 
-	<div class="inline-flex min-w-[12rem] flex-col space-y-2 bg-gray-400 p-4">
+	<div class="inline-flex min-w-[12rem] grow flex-col space-y-2 bg-gray-400 p-4">
 		<span class="text-center font-bold uppercase">Codeball Jobs</span>
 		<div class="space-x-1 text-center">
 			<span class="text-6xl text-white">{statNumberOfJobs}</span><span />
 		</div>
 	</div>
 
-	<div class="inline-flex min-w-[12rem] flex-col space-y-2 bg-gray-400 p-4">
+	<div class="inline-flex min-w-[12rem] grow flex-col space-y-2 bg-gray-400 p-4">
 		<span class="text-center font-bold uppercase">Approvals</span>
 		<div class="space-x-1 text-center">
 			<span class="text-6xl text-white">{statApprovedContributions}</span><span />
 		</div>
 	</div>
 
-	<!--
-	<div class="inline-flex min-w-[12rem] flex-col space-y-2 bg-gray-400 p-4">
-		<span class="text-center font-bold uppercase">avg Lead Time</span>
-		<div class="space-x-1 text-center">
-			<span class="text-6xl text-white">{statAvgTimeToMerge}</span><span>m</span>
+	<div class=" flex min-w-[12rem] grow flex-col gap-2">
+		<div class="inline-flex min-w-[12rem] flex-col space-y-2 bg-gray-400 p-4">
+			<span class="text-center font-bold uppercase">Avg. Lead Time</span>
+			<div class="space-x-1 text-center">
+				<Time seconds={timeToMergeAvgSeconds} />
+			</div>
+			<span class="text-center text-sm">Total</span>
+		</div>
+
+		<div class="inline-flex min-w-[12rem] flex-col space-y-2 bg-gray-400 p-4">
+			<div class="space-x-1 text-center">
+				<Time seconds={timeToMergeApprovedByCodeballAvgSeconds} />
+			</div>
+			<span class="text-center text-sm">With Codeball</span>
 		</div>
 	</div>
 
-	<div class="inline-flex min-w-[12rem] flex-col space-y-2 bg-gray-400 p-4">
-		<span class="text-center font-bold uppercase">mean Lead Time</span>
-		<div class="space-x-1 text-center">
-			<span class="text-6xl text-white">{statMeanTimeToMerge}</span><span>m</span>
+	<div class=" flex min-w-[12rem] grow flex-col gap-2">
+		<div class="inline-flex flex-col space-y-2 bg-gray-400 p-4">
+			<span class="text-center font-bold uppercase">Median Lead Time</span>
+			<div class="space-x-1 text-center">
+				<Time seconds={timeToMergeMedianSeconds} />
+			</div>
+			<span class="text-center text-sm">Total</span>
+		</div>
+
+		<div class="inline-flex flex-col space-y-2 bg-gray-400 p-4">
+			<div class="space-x-1 text-center">
+				<Time seconds={timeToMergeApprovedByCodeballMedianSeconds} />
+			</div>
+			<span class="text-center text-sm">With Codeball</span>
 		</div>
 	</div>
 
-	<div class="inline-flex min-w-[12rem] flex-col space-y-2 bg-gray-400 p-4">
-		<span class="text-center font-bold uppercase">Time Saved</span>
-		<div class="space-x-1 text-center">
-			<span class="text-6xl text-white">{statTimeSaved}</span><span>m</span>
+	<div class=" flex min-w-[12rem] grow flex-col gap-2">
+		<div class="inline-flex flex-col space-y-2 bg-gray-400 p-4">
+			<span class="text-center font-bold uppercase">Codeball</span>
+			<div class="space-x-1 text-center">
+				<span class="text-6xl text-white">{mergedContributionJobsApprovedByCodeball.length}</span>
+			</div>
+			<span class="text-center text-sm">Approved</span>
 		</div>
-	</div>-->
+
+		<div class="inline-flex  flex-col space-y-2 bg-gray-400 p-4">
+			<div class="space-x-1 text-center">
+				<span class="text-6xl text-white">{mergedContributionJobsRejectedByCodeball.length}</span>
+			</div>
+			<span class="text-center text-sm">Not Approved</span>
+		</div>
+	</div>
 </div>
---
+++ src/lib/components/dashboard/Time.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let seconds: number;
+
+	$: minutes = seconds / 60;
+	$: hours = minutes / 60;
+</script>
+
+{#if hours > 1}
+	<span class="text-6xl text-white">{hours.toFixed(1)}</span><span>h</span>
+{:else}
+	<span class="text-6xl text-white">{minutes.toFixed(0)}</span><span>min</span>
+{/if}
