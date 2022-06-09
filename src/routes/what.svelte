a3f0cbb25f9b9345b688e0de819599445139f95f
Gustav Westling <gustav@getsturdy.com>
2022-06-09 18:01:29 +0200

    Smaller design tweaks to /what

--- src/routes/what.svelte
+++ src/routes/what.svelte
@@ -20,69 +20,74 @@
 
 	<div class="my-8 flex flex-col gap-16 lg:flex-row">
 		<div class="flex flex-col gap-8">
-			<div class="text-md text-gray-700 lg:grid lg:grid-cols-3 lg:gap-2">
+			<div class="text-md text-gray-700 lg:grid lg:grid-cols-3">
 				<div class="col-start-2 hidden text-center text-2xl font-bold text-gray-700 lg:block">
 					CARS 🚙
 				</div>
 				<div class="hidden text-center text-2xl font-bold  text-gray-700 lg:block">CODE 💻</div>
 
-				<div class="bg-gray-300 p-2 py-5 lg:bg-transparent">
+				<div class="bg-gray-300 p-2 py-5 lg:bg-transparent lg:p-4">
 					<div class="text-4xl font-bold text-gray-700 lg:text-2xl">L0</div>
 					<div class="text-gray-500">No automation</div>
 				</div>
-				<div class="p-2">
+				<div class="p-2 lg:p-4">
 					<strong class="lg:hidden">🚙</strong>
 					<p>You drive the car (like a savage). Crashes happen.</p>
 				</div>
-				<div class="p-2">
+				<div class="p-2 lg:p-4">
 					<strong class="lg:hidden">💻</strong>
 					<p>No assistance. Humans perform all code checks out of memory. Bugs happen.</p>
 				</div>
-				<div class="bg-gray-300 p-2 py-5 lg:bg-transparent">
+
+				<div class="bg-gray-300 p-2 py-5 lg:bg-gray-200 lg:p-4">
 					<div class="text-4xl font-bold text-gray-700 lg:text-2xl">L1</div>
 					<div class="text-gray-500">Some assistance</div>
 				</div>
-				<div class="p-2">
+				<div class="p-2 lg:bg-gray-200 lg:p-4">
 					<strong class="lg:hidden">🚙</strong>
 					<p>Cruise control. Or lane assist. But not both, that's L2.</p>
 				</div>
-				<div class="p-2">
+				<div class="p-2 lg:bg-gray-200 lg:p-4">
 					<strong class="lg:hidden">💻</strong>
 					<p>
 						Rule-based tools that identify common mistakes.
-						<br /><br />
-						Example: code linters and formatters.
+						<br />
+						<span class="text-gray-400">Example: Code linters and formatters.</span>
 					</p>
 				</div>
-				<div class="bg-gray-300 p-2 py-5 lg:bg-transparent">
+
+				<div class="bg-gray-300 p-2 py-5 lg:bg-transparent lg:p-4">
 					<div class="text-4xl font-bold text-gray-700 lg:text-2xl">L2</div>
 					<div class="text-gray-500">Partial automation</div>
 				</div>
-				<div class="p-2">
+				<div class="p-2 lg:p-4">
 					<strong class="lg:hidden">🚙</strong>
 					<p>Both cruse control and lane assist, with some smart breaking sprinkled in.</p>
 				</div>
-				<div class="p-2">
+				<div class="p-2 lg:p-4">
 					<strong class="lg:hidden">💻</strong>
 					<p>
-						Tools aware of the immediate code context.<br /><br />
-						Example: Auto-complete, Refactoring suggestions, static code analysis.
+						Tools aware of the immediate code context.<br />
+						<span class="text-gray-400"
+							>Example: Auto-complete, Refactoring suggestions, static code analysis.</span
+						>
 					</p>
 				</div>
-				<div class="bg-gray-300 p-2 py-5 lg:bg-transparent">
+
+				<div class="bg-gray-300 p-2 py-5 lg:bg-gray-200 lg:p-4 ">
 					<div class="text-4xl font-bold text-gray-700 lg:text-2xl">L3</div>
 					<div class="text-gray-500">Conditional automation</div>
 				</div>
-				<div class="p-2">
+				<div class="p-2 lg:bg-gray-200 lg:p-4">
 					<strong class="lg:hidden">🚙</strong>
 					<p>
 						An AI drives the car when the conditions are right. You have to take over if the car
 						gets confused.
 						<br />
-						Example: Tesla
+						<span class="text-gray-400">Example: Tesla</span>
 					</p>
 				</div>
-				<div class="p-2">
+				<div class="p-2 lg:bg-gray-200 lg:p-4">
 					<strong class="lg:hidden">💻</strong>
 					<p>
 						The AI reviews and approves code contributions based on the repository context. You have
@@ -90,19 +95,19 @@
 						<strong class="text-gray-700">CODEBALL-1</strong> is here.
 					</p>
 				</div>
-				<div class="bg-gray-300 p-2 py-5 lg:bg-transparent">
+
+				<div class="bg-gray-300 p-2 py-5 lg:bg-transparent lg:p-4">
 					<div class="text-4xl font-bold text-gray-700 lg:text-2xl">L4</div>
 					<div class="text-gray-500">High automation</div>
 				</div>
-				<div class="p-2">
+				<div class="p-2 lg:p-4">
 					<strong class="lg:hidden">🚙</strong>
 					<p>
-						You can Netflix and relax while the car drives you, but it doesn't go everywhere.<br
-						/><br />
-						Example: Waymo
+						You can Netflix and relax while the car drives you, but it doesn't go everywhere.<br />
+						<span class="text-gray-400">Example: Waymo</span>
 					</p>
 				</div>
-				<div class="p-2">
+				<div class="p-2 lg:p-4">
 					<strong class="lg:hidden">💻</strong>
 					<p>
 						The AI informs you of the exact impact of your code based on the production context. It
@@ -110,15 +115,15 @@
 					</p>
 				</div>
 
-				<div class="bg-gray-300 p-2 py-5 lg:bg-transparent">
+				<div class="bg-gray-300 p-2 py-5 lg:bg-gray-200 lg:p-4">
 					<div class="text-4xl font-bold text-gray-700 lg:text-2xl">L5</div>
 					<div class="text-gray-500">Full automation</div>
 				</div>
-				<div class="p-2">
+				<div class="p-2 lg:bg-gray-200 lg:p-4">
 					<strong class="lg:hidden">🚙</strong>
 					<p>Goes everywhere by itself. <br />Basically, Skynet.</p>
 				</div>
-				<div class="p-2">
+				<div class="p-2 lg:bg-gray-200 lg:p-4">
 					<strong class="lg:hidden">💻</strong>
 					<p>You are now unemployed.</p>
 				</div>
