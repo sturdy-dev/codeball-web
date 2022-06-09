fadf7938f5debcc7abd56b8fae79a260efd248ae
Gustav Westling <gustav@getsturdy.com>
2022-06-09 12:42:06 +0200

    Add pricing page

---
+++ src/routes/pricing.svelte
@@ -0,0 +1,51 @@
+<article class="font-mono">
+	<h1 class="text-6xl font-black tracking-tight sm:text-8xl md:text-9xl">Pricing</h1>
+
+	<div class="my-8 flex flex-col gap-16 lg:flex-row">
+		<div class="flex flex-col gap-8">
+			<p class="text-xl text-gray-500 sm:max-w-xl">What does an AI even cost these days?</p>
+
+			<div class="flex space-x-8">
+				<div class="space-y-8 bg-gray-800 p-8 text-white">
+					<h2 class="text-6xl font-light">Free</h2>
+					<ul class="list-inside list-disc">
+						<li>Free for open source projects</li>
+						<li>No limits</li>
+					</ul>
+				</div>
+
+				<div class="space-y-8 bg-gray-800 p-8 text-white">
+					<h2 class="text-6xl font-black">Deluxe edition</h2>
+					<ul class="list-inside list-disc">
+						<li>Private repositories</li>
+						<li>Really nice dashboards</li>
+						<li>Unlimited repositories</li>
+						<li>No usage limits</li>
+					</ul>
+
+					<div>
+						<p>$10 / user / month</p>
+						<p class="text-gray-300">14 day free trial, no credit card required</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="text-gray-600">
+				<p>
+					Get started with the <strong>Deluxe edition</strong> by adding the Codeball GitHub Action to
+					your private repository.
+				</p>
+				<p>
+					When the free trial is about to end, we'll send you a payment link to take your hard
+					earned cash.
+				</p>
+			</div>
+
+			<ul class="flex gap-2">
+				<li>[<a href="/">index</a>]</li>
+				<li>[<a href="/how">how</a>]</li>
+				<li>[<a href="https://github.com/sturdy-dev/codeball-action">github action</a>]</li>
+			</ul>
+		</div>
+	</div>
+</article>
