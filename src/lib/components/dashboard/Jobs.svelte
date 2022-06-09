354a21f4a6b2627d8b7837d25f69abd71f39e3b1
Gustav Westling <gustav@getsturdy.com>
2022-06-09 15:24:25 +0200

    Show executed actions on the dashboard

--- src/lib/components/dashboard/Jobs.svelte
+++ src/lib/components/dashboard/Jobs.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { type ContributionJob, type Job } from '$lib/jobs';
+	import { type ContributionJob, type Event, type Job } from '$lib/jobs';
 	import { formatDistance } from 'date-fns';
 
 	export let jobs: Job[];
@@ -49,6 +49,12 @@
 		return getRelativeTime(new Date(c.contribution.created_at));
 	};
 
+	const gitHubActionEvents = (c: ContributionJob): Event[] => {
+		const events = c?.events?.filter((e) => e.type === 'github_action') || [];
+
+		return events.sort((a, b) => a.created_at.localeCompare(b.created_at));
+	};
+
 	$: latestJobsForContribution = getLatestByURL(jobsByContributionURL).sort(byPredictedAt);
 
 	$: showJobs = latestJobsForContribution;
@@ -67,51 +73,77 @@
 		{@const isMerged = job.contribution.actual_outcome?.merged_at}
 		{@const isClosed = job.contribution.actual_outcome?.closed_at}
 		{@const isOpen = !job.contribution.actual_outcome}
+		{@const events = gitHubActionEvents(job)}
 
-		<div class="flex flex-col items-start gap-6 p-3 sm:flex-row">
-			<div class="flex grow flex-col">
-				<a class="overflow-hidden text-sm text-gray-900" href={job.contribution.url}>
-					<code class="text-gray-500">#{job.contribution.number}</code>
-					{job.contribution.title}
-				</a>
+		<div class="space-y-2 p-3">
+			<div class="flex flex-col items-start gap-6 sm:flex-row">
+				<div class="flex grow flex-col">
+					<a class="overflow-hidden text-sm text-gray-900" href={job.contribution.url}>
+						<code class="text-gray-500">#{job.contribution.number}</code>
+						{job.contribution.title}
+					</a>
 
-				<span class="text-sm text-gray-500">{contributionCreatedAtDuration(job)}</span>
-			</div>
+					<span class="text-sm text-gray-500">{contributionCreatedAtDuration(job)}</span>
+				</div>
 
-			<div class="">
-				<span
-					class="block w-32 py-2 text-center text-xs font-semibold leading-5"
-					class:bg-green-100={codeballApproved}
-					class:text-green-800={codeballApproved}
-				>
-					{codeballApproved ? 'CODEBALL 👍' : 'NOT APPROVED'}
-				</span>
-			</div>
-
-			<div class="space-y-1">
-				<span
-					class="block w-32 py-2 text-center text-xs font-semibold leading-5"
-					class:bg-purple-100={isMerged}
-					class:text-purple-800={isMerged}
-					class:bg-red-100={isClosed}
-					class:text-red-800={isClosed}
-					class:bg-green-100={isOpen}
-					class:text-green-800={isOpen}
-				>
-					{#if isMerged}
-						MERGED
-					{:else if isClosed}
-						CLOSED
-					{:else}
-						OPEN
-					{/if}
-				</span>
-				{#if isMerged}
-					<span class="block text-center text-xs text-gray-400">
-						{contributionOpenDuration(job)}
+				<div>
+					<span
+						class="block w-32 py-2 text-center text-xs font-semibold leading-5"
+						class:bg-green-100={codeballApproved}
+						class:text-green-800={codeballApproved}
+					>
+						{codeballApproved ? 'CODEBALL 👍' : 'NOT APPROVED'}
 					</span>
-				{/if}
+				</div>
+
+				<div class="space-y-1">
+					<span
+						class="block w-32 py-2 text-center text-xs font-semibold leading-5"
+						class:bg-purple-100={isMerged}
+						class:text-purple-800={isMerged}
+						class:bg-red-100={isClosed}
+						class:text-red-800={isClosed}
+						class:bg-green-100={isOpen}
+						class:text-green-800={isOpen}
+					>
+						{#if isMerged}
+							MERGED
+						{:else if isClosed}
+							CLOSED
+						{:else}
+							OPEN
+						{/if}
+					</span>
+					{#if isMerged}
+						<span class="block text-center text-xs text-gray-400">
+							{contributionOpenDuration(job)}
+						</span>
+					{/if}
+				</div>
 			</div>
+
+			{#if events.length > 0}
+				<div>
+					<span class="text-sm text-gray-400">Actions</span>
+					<div class="flex space-x-2">
+						{#each events as event, i}
+							{@const isLast = i === events.length - 1}
+							<div
+								class="p-2"
+								class:bg-red-100={event.github_action.error !== null}
+								class:text-red-800={event.github_action.error !== null}
+								class:bg-green-100={event.github_action.error === null}
+								class:text-green-800={event.github_action.error === null}
+							>
+								{event.github_action.name}
+							</div>
+							{#if !isLast}
+								<div class="py-2">👉</div>
+							{/if}
+						{/each}
+					</div>
+				</div>
+			{/if}
 		</div>
 	{/each}
 </section>
