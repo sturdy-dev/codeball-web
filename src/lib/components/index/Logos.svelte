cf8e79bf13e70017f8929318574277eb43b7642b
Kiril Videlov <kiril@videlov.com>
2022-06-17 10:50:28 +0200

    Index: Add EMQX logo

--- src/lib/components/index/Logos.svelte
+++ src/lib/components/index/Logos.svelte
@@ -5,6 +5,7 @@
 	import KitemakerLogo from '$lib/assets/logo-kitemaker.png';
 	import GogsLogo from '$lib/assets/logo-gogs.png';
 	import MasonLogo from '$lib/assets/logo-mason.svg';
+	import EMQXLogo from '$lib/assets/logo-emqx.png';
 
 	const logos = [
 		{ src: SturdyLogo, alt: 'Sturdy' },
@@ -12,14 +13,15 @@
 		{ src: OryLogo, alt: 'Ory' },
 		{ src: KitemakerLogo, alt: 'Kitemaker' },
 		{ src: GogsLogo, alt: 'Gogs' },
-		{ src: MasonLogo, alt: 'Mason' }
+		{ src: MasonLogo, alt: 'Mason' },
+		{ src: EMQXLogo, alt: 'EMQX' }
 	];
 </script>
 
 <div class="flex flex-col gap-2">
 	<span class="text-md text-gray-500">Used by</span>
 
-	<div class="flex flex-wrap gap-x-8 gap-y-2 grayscale">
+	<div class="flex flex-wrap gap-x-6 gap-y-2 grayscale">
 		{#each logos as { src, alt }}
 			<img class="h-6" {src} {alt} />
 		{/each}
