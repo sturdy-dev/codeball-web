79bc211f515a2c542871f1d31ae14ffbc9004aae
Gustav Westling <gustav@getsturdy.com>
2022-06-17 15:35:29 +0200

    Updated supabase blog post

--- src/routes/blog/supabase-deepdive.svelte
+++ src/routes/blog/supabase-deepdive.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import imgBallWithBrain from '$lib/assets/BallWithBrainRaster.png';
+	import Button from '$lib/Button.svelte';
 </script>
 
-<div class="prose">
-	<h1>Deepdive and explanation of Codeball for Supabase.</h1>
+<div class="prose mb-4">
+	<h1>Codeball AI Code Review, a deepdive into the numbers</h1>
 
 	<p>
 		<a href="/">Codeball</a> is an AI trained to perform code review, and the first available
@@ -11,10 +11,15 @@
 	</p>
 
 	<p>
+		What happens when you add Codeball to your project, is it safe, and can it even save you money?
+		Let's dig into the numbers from the default Codeball demo from the Codeball index page.
+	</p>
+
+	<p>
 		Supabase is a well-known open-source project mainly built and maintained by employees of the
 		company with the same name. We've used Supabase as the default example for the Codeball demo
 		site, because Supabase is an open-source project that's largely run like any project at
-		closed-source companies
+		closed-source companies <span class="text-gray-500">(business === money)</span>
 		<span class="text-gray-500">(and because the results are pretty good)</span>.
 	</p>
 
@@ -46,19 +51,23 @@
 		</li>
 	</ul>
 
+	<div class="flex w-full items-center">
+		<div class="bg-green-300 text-center text-green-800" style="width: 72%">Approved – 72%</div>
+		<div class="flex-1 bg-red-300 text-center text-red-800">Got feedback</div>
+	</div>
+
 	<p>
 		Codeball <strong>correctly</strong> predicted the instant-YOLO-merge for 14 out of the 36 Pull Requests,
 		and did not approve a single PR that was rejected or further iterated on.
 	</p>
 
-	<p>
-		Codeball was unsure about 18 PRs that ended up getting merged directly, and opted to not merge
-		them.
-	</p>
+	<div class="flex w-full items-center">
+		<div class="bg-green-300 text-center text-green-800" style="width: 28%">Approved – 28%</div>
+		<div class="flex-1 bg-blue-300 text-center text-blue-800">Not Approved</div>
+	</div>
 
-	<p class="border-l-4 border-black py-1 pl-4">
-		Sidenote: Kudos to Supabase for having such a high approval rate, and zero-to-no bike shedding
-		going on in the PRs! A 72% instant-LGTM ratio is higher than the industry average of 65%.
+	<p>
+		Codeball was unsure about 18 PRs that ended up getting merged directly, did not approve them.
 	</p>
 
 	<p>
@@ -114,10 +123,10 @@
 		"So Codeball is like a strict bartender who only serves you when they are absolutely sure you're
 		old enough. You may still be overage but Codeball's not serving you."
 		<br />
-		– <a href="https://news.ycombinator.com/item?id=31534424">apugoneappu on Hacker News</a>
+		— <a href="https://news.ycombinator.com/item?id=31534424">apugoneappu on Hacker News</a>
 	</p>
 
-	<h2>Why do some PRs get approved?</h2>
+	<h2>What can we tell about Codeball's performance?</h2>
 
 	<p>
 		Codeball calculates many different characteristics ("features") for a PR. This includes things
@@ -125,87 +134,91 @@
 		contributions to the modified files, the authors history with the repository and nearby
 		directories, as well as the health of the files and repository itself
 		<span class="text-gray-500">
-			(+ a few other things, but that's a business secret apparently)
+			(+ a few other things, but what they are is apparently a business secret)
 		</span>.
+	</p>
+
+	<p class="border-l-4 border-black py-1 pl-4">
+		Almost all Pull Requests ever created end up getting merged sooner or later, but some require
+		one or many rounds of feedback and discussions before it's ready.<br /><br />
+		In this blog post, "merged" and "approved" means that the first version of the Pull Request was approved
+		and merged, and that the only feedback it got was some form of "LGTM 🚀🌕".
 	</p>
 
 	<p>
 		Honestly it's a bit of a black box, and I'm just here to write a blog post. So this will be a
-		bit of guessing, and me going through all 50 PRs manually
-		<span class="text-gray-500">(this was a very intimate exercise)</span>
-		to try to identify indicators to look at, and why Codeball potentially might have approved the PR.
+		bit of guessing, and me going through all 50 PRs manually to try to characterize the PRs and see
+		if I can find any patterns <span class="text-gray-500"
+			>(this was a very intimate exercise, it feels live I've gotten to know all core contributors)</span
+		>.
 	</p>
 
 	<ul>
 		<li>
-			11 PRs only changed files that where almost entirely or entirely
-			<strong
-				>created by the PR author</strong
+			<strong>11</strong> PRs changed files that where entirely or almost entirely
+			<strong>created by the PR author</strong>. These are often smaller improvements or fixes, and
+			generally safer than the average Pull Request, as the author is already familiar with the
+			code. 9 out of 11 PRs <span class="text-gray-500">(81%)</span> in this category was approved without
+			further feedback.
+		</li>
+		<li>
+			The average number of <strong>files modified per PR is 4.6</strong>. The PRs that where
+			approved on the first iteration had an average of <strong>3.79</strong> files per PR.
+		</li>
+		<li>
+			26 PRs <strong>changed only a single file</strong>, 24 of which where approved, including 6
+			PRs from external and first-time contributors.
+		</li>
+		<li>
+			19 PRs changed more than 1 file. 12 of these where YOLO-approved by the team <span
+				class="text-gray-500">(being YOLO is a great thing)</span
 			>.
-			And included many "quick fixes" to earlier PRs.<br />
-			9 <span class="text-gray-500">(81%)</span> of these where approved by the humans, and 5
-			<span class="text-gray-500">(45%)</span> where approved by Codeball. The recall here is 55%. It's
-			higher than normal, but not abnormally so.
 		</li>
-		<li>
-			The average number of <strong>files modified per PR is 4.6</strong> For the ones that Codeball
-			approved, the average is 1.41, and for the human approved it's a bit higher at 3.79.
-		</li>
-		<li>
-			26 PRs <strong>changed only a single file</strong> 24 of these where instantly approved by the
-			humans
-			<span class="text-gray-500">(including 6 PRs from external and first time contributors)</span
-			>. Codeball approved 11 <span class="text-gray-500">(recall: 45%)</span> of the PRs with only 1
-			file changed.
-		</li>
-		<li>
-			19 PRs changed more than 1 file. 12 of these where YOLO-approved by the team, Codeball only
-			approved 3 of them <span class="text-gray-500">(recall: 25%)</span>. Codeball approved 3 PRs
-			that had more than 1 file changed:
-		</li>
-		<ul>
-			<!-- 7128 -->
-			<li>
-				One bugfix with added tests, 4 files changed in total, all files where previously created by
-				the same author.
-			</li>
-			<!-- 7201 -->
-			<li>
-				One bugfix with deleted code, 2 files changed in total, none of the files where previously
-				edited by the author.
-			</li>
-			<!-- 7262 -->
-			<li>
-				One bugfix with added code, 2 files chained in total, all files where previously created by
-				the same author.
-			</li>
-		</ul>
 	</ul>
 
 	<p>
-		This indicates that while Codeball never guessed anything wrong, tt's not very confident when
-		predicting larger Pull Requests (it's a chicken).
+		There is a clear trend that "smaller is better", which makes sense, a smaller PR has few lines
+		of code that could potentially screw something up. And a bugfix is probably smaller (measured in
+		LOC) than a feature addition.
+	</p>
+
+	<p>
+		Codeball seems to agree, and picks up on this trend as well. Codeball has a higher-than-average
+		recall for Pull Requests in the self-authored category (45%) and single-file-changed category
+		(81%).
+	</p>
+
+	<p>
+		What's cool is that while there is strong indicator that "smaller is better", not all small PRs
+		where approved, and that Codeball was able to identify the PRs that should not be approved
+		as-is, and for this demo, did not make a single mistake.
+	</p>
+
+	<p>
+		For the latest 50 PRs to supabase/supabase the precision is 100%. Internally however, with much
+		larger test set, there are of-course some false-positives, and the precision is around 99%.
 	</p>
 
 	<p class="border-l-4 border-black py-1 pl-4">
-		Codeball has a higher accuracy (~99%) than the
+		Codeball has a higher precision (~99%) than the
 		<a
 			href="https://www.nhs.uk/conditions/contraception/male-condoms/#:~:text=When%20used%20correctly%20every%20time,are%20used%20as%20contraception."
-			>
+		>
 			efficacy of condoms
 		</a>
 		(98%). Similarly to the usage of condoms, both have a "Plan B" readily available
 		<sup><a id="back-ref-1" href="#ref-1">1</a></sup>
-		(revert the PR).
+		(identify the oopsie-doopsie and revert).
 	</p>
 
-	<h2>What benefits would usage of Codeball have for the Supabase team?</h2>
+	<h2>Can Codeball save money for Supabase?</h2>
 
 	<p>
-		The Supabase team are pretty diligent, and are usually pretty fast to approve PRs. There might
-		be a lot of spam of the "hey plzzz approve my PR"-kind happening on their private Slack or
-		something, but we can't really see it.
+		In this world, time is money, and money is time. And it's popular to track metrics like "change
+		lead time" etc etc.
 	</p>
+
+	<p>Out of the 50 latest PRs in the repository:</p>
 
 	<ul>
 		<li>
@@ -213,50 +226,56 @@
 			<strong>within 1 hour</strong>
 		</li>
 		<li>
-			22 <span class="text-gray-500">(44%)</span> PRs had to wait between 1 and 24 hours to receive their
-			first review
+			22 <span class="text-gray-500">(44%)</span> PRs had to wait
+			<strong>between 1 and 24 hours</strong> to receive their first review
 		</li>
 		<li>
-			12 <span class="text-gray-500">(24%)</span> PRs waited for more than 24 hours to receive their
-			first review
+			12 <span class="text-gray-500">(24%)</span> PRs waited for <strong>more than 24 hours</strong>
+			to receive their first review
 		</li>
 	</ul>
 
 	<p>
-		The slowest PR was open for 5 days before getting the first review (which Codeball correctly
-		approved btw 😉).
+		The slowest PR was open for 5 days before getting the first review <span class="text-gray-500"
+			>(which Codeball correctly approved btw 😉)</span
+		>.
 	</p>
 
 	<p>
-		The time-to-merge for the Pull Requests that Codeball approved was spread out in buckets of
-		roughly the same size (25% / 50% / 25%).
-		<!---
-            <a href="https://github.com/supabase/supabase/pull/7128">#7128</a>,
-            <a href="https://github.com/supabase/supabase/pull/7176">#7176</a>,
-            <a href="https://github.com/supabase/supabase/pull/7203">#7203</a>,
-        --->
+		The time-to-merge for the Pull Requests that Codeball would have approved was spread out in
+		buckets of roughly the same size (25% / 50% / 25%).
 	</p>
 
 	<p>
-		We won't go into the details of why faster-merge-is-better and will save you 💰💸🤑💲. Others
-		have already done that, and it's boring....
+		It's hard to measure what the associated cost is to merge a PR after 24 hours instead of 15
+		minutes, but it's safe to say that it's above 0.
 	</p>
 
 	<p>
-		One way to apply CODEBALL-1 to your repository is to allow Codeball to approve incoming PRs that
-		are safe, and with an immediate review (running Codeball takes just a few seconds), merge the PR
-		right away if Codeball considers it safe.
+		What's easier to argue for is that, for each of these PRs, someone had to interact with it at
+		least three times: The author to create it, the reviewer to review it, and then for the author
+		to go back to it to merge it.
 	</p>
 
 	<p>
-		When used in this setup, you'll both save the time wasted waiting for review (and ship features
-		and fixes faster), as well as save time wasted when context switching from coding on one task,
-		to reviewing, and then back again.
+		Each of these interactions takes a small amount of time, maybe just a minute or two, but they
+		all add up, and then we haven't even included the cost of context switching.
 	</p>
 
 	<p>
-		By Gustav and the Codeballers<br>
-		16th of June 2022
+		With an epic AI such as Codeball, it's possible to get the number of interactions required to
+		merge a PR down to just one, and reducing the "time to merge" to just a few seconds.
+	</p>
+
+	<p>So yes, there is money to be saved here.</p>
+</div>
+
+<Button href="/">HIRE A BOT</Button>
+
+<div class="prose mt-4">
+	<p class="text-blue-600">
+		With 🧠 from the Codeballers,<br />
+		17th of June 2022
 	</p>
 
 	<hr />
@@ -264,8 +283,7 @@
 	<ul>
 		<li class="text-sm text-gray-500 ">
 			Codeball is not affiliated with Supabase, we're just fans. Or is this an elaborate attempt to
-			try to get Supabase to install Codeball? We should get something from this free marketing
-			we're giving them....
+			try to get Supabase to install Codeball?
 		</li>
 		<li class="text-sm text-gray-500">
 			In some calculations above, some PRs from dependabot have been excluded, as those generally
