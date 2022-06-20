3ba909f7b05cdf8b5161ba21e9c793ad1e3fa04e
Kiril Videlov <kiril@videlov.com>
2022-06-20 09:45:03 +0200

    Index: Add Simplepoll's logo

--- src/lib/components/index/Logos.svelte
+++ src/lib/components/index/Logos.svelte
@@ -6,6 +6,7 @@
 	import GogsLogo from '$lib/assets/logo-gogs.png';
 	import MasonLogo from '$lib/assets/logo-mason.svg';
 	import EMQXLogo from '$lib/assets/logo-emqx.png';
+	import SimplepollLogo from '$lib/assets/logo-simplepoll.svg';
 
 	const logos = [
 		{ src: SturdyLogo, alt: 'Sturdy' },
@@ -14,14 +15,15 @@
 		{ src: KitemakerLogo, alt: 'Kitemaker' },
 		{ src: GogsLogo, alt: 'Gogs' },
 		{ src: MasonLogo, alt: 'Mason' },
-		{ src: EMQXLogo, alt: 'EMQX' }
+		{ src: EMQXLogo, alt: 'EMQX' },
+		{ src: SimplepollLogo, alt: 'Simplepoll' },
 	];
 </script>
 
 <div class="flex flex-col gap-2">
 	<span class="text-md text-gray-500">Used by</span>
 
-	<div class="flex flex-wrap gap-x-6 gap-y-2 grayscale">
+	<div class="flex flex-wrap gap-x-10 gap-y-6 grayscale">
 		{#each logos as { src, alt }}
 			<img class="h-6" {src} {alt} />
 		{/each}
