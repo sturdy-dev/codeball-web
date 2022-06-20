b34d67608f2a5596fc9e6328366d64b8500054e8
Gustav Westling <gustav@getsturdy.com>
2022-06-20 15:20:07 +0200

    confidences: handle old jobs without data

--- src/lib/components/contributions/Files.svelte
+++ src/lib/components/contributions/Files.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
 	import { type ContributionJob } from '$lib/jobs';
+
 	export let job: ContributionJob;
 
-	$: files = job.contribution?.files.map((file, idx) => {
-		return {
-			name: file.name,
-			confidence: job.contribution.predicted_outcome.file_probabilities[idx]
-		};
-	});
+	const build = (job: ContributionJob) => {
+		if (!job.contribution.files) {
+			return [];
+		}
+
+		if (
+			job.contribution.files.length !== job.contribution.predicted_outcome.file_probabilities.length
+		) {
+			return [];
+		}
+
+		return job.contribution?.files.map((file, idx) => {
+			return {
+				name: file.name,
+				confidence: job.contribution.predicted_outcome.file_probabilities[idx]
+			};
+		});
+	};
+
+	$: files = build(job);
 </script>
 
 <div class="flex flex-col">
@@ -21,5 +36,7 @@
 			<span>{file.name}</span>
 			<span>{file.confidence.toFixed(2)}</span>
 		</div>
+	{:else}
+		<div class="text-center">No files found :-(</div>
 	{/each}
 </div>
