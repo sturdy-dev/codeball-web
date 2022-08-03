6115704dba8cba57283dc53cb27bae7949328900
Gustav Westling <gustav@getsturdy.com>
2022-08-03 15:49:52 +0200

    all: css fixes and remove hubspot chat

--- src/lib/Separator.svelte
+++ src/lib/Separator.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
     export let up = false;
+    $: rotate = up ? "-rotate-[4deg]" : "rotate-[4deg]";
 </script>
 
-<div class="h-1 bg-gradient-to-r from-transparent via-orange-200 to-transparent  -ml-40 -mr-40 rounded-md"
-     class:-rotate-[4deg]={up}
-     class:rotate-[4deg]={!up}
-></div>
+<div class="{'h-1 bg-gradient-to-r from-transparent via-orange-200 to-transparent -ml-40 -mr-40 rounded-md ' + rotate}"></div>
