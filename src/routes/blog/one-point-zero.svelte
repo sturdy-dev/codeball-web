54b6ae83318b59339f98889d9a7d6e63ba1ba99c
Gustav Westling <gustav@getsturdy.com>
2022-08-11 12:31:07 +0200

    all: format

--- src/routes/blog/one-point-zero.svelte
+++ src/routes/blog/one-point-zero.svelte
@@ -108,7 +108,11 @@
 
 	<h2>Read more</h2>
 	<ul>
-		<li><a href="/blog/waiting-for-code-review">You are waiting for code review more than you should</a></li>
+		<li>
+			<a href="/blog/waiting-for-code-review"
+				>You are waiting for code review more than you should</a
+			>
+		</li>
 		<li><a href="/blog/supabase-deepdive">Codeball x Supabase Deepdive</a></li>
 	</ul>
 </div>
