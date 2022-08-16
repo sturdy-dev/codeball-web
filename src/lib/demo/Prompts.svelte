dabb40c29214b6bf664bb913624a35d063841cb7
Gustav Westling <gustav@getsturdy.com>
2022-08-16 13:23:38 +0200

    demo: start working on a codex demo

---
+++ src/lib/demo/Prompts.svelte
@@ -0,0 +1,21 @@
+<script lang="js">
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+	import Prompt from './Prompt.svelte';
+
+	function pickPrompt(id) {
+		dispatch('pickedPrompt', { id });
+	}
+
+	export let selectedPrompt;
+	export let prompts;
+</script>
+
+<div class="text-center font-bold">Pick your comment</div>
+{#each prompts as prompt}
+	<div on:click={() => pickPrompt(prompt.id)}>
+		<Prompt selected={selectedPrompt === prompt.id}>
+			"{prompt.text}"
+		</Prompt>
+	</div>
+{/each}
