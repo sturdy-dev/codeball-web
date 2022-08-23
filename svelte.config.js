d2434e523d2101a1a88dd4470c8cdad4c935749c
Nikita Galaiko <nikita@galaiko.rocks>
2022-08-23 10:56:43 +0200

    csp: disable for dev

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
@@ -10,21 +12,24 @@ const config = {
 		prerender: {
 			default: true
 		},
-		csp: {
-			directives: {
-				'default-src': ['self'],
-				'connect-src': [
-					'self',
-					'api.codeball.ai',
-					'umami.codeball.ai',
-					'app.posthog.com',
-					'vitals.vercel-insights.com'
-				],
-				'img-src': ['self', 'api.producthunt.com'],
-				'child-src': ['self', 'ghbtns.com'],
-				'script-src': ['self', 'umami.codeball.ai', 'app.posthog.com']
-			}
-		}
+
+		csp: isProduction
+			? {
+					directives: {
+						'default-src': ['self'],
+						'connect-src': [
+							'self',
+							'api.codeball.ai',
+							'umami.codeball.ai',
+							'app.posthog.com',
+							'vitals.vercel-insights.com'
+						],
+						'img-src': ['self', 'api.producthunt.com'],
+						'child-src': ['self', 'ghbtns.com'],
+						'script-src': ['self', 'umami.codeball.ai', 'app.posthog.com']
+					}
+			  }
+			: undefined
 	}
 };
 
