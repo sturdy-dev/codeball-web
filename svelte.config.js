d8471775a092ca8b1149a7c6d8544e1ba23e1c78
Nikita Galaiko <nikita@galaiko.rocks>
2022-08-24 11:05:26 +0200

    csp: remove

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
-const isProduction = process.env.NODE_ENV === 'production'
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
@@ -11,25 +9,7 @@ const config = {
 		adapter: adapter(),
 		prerender: {
 			default: true
-		},
-
-		csp: isProduction
-			? {
-					directives: {
-						'default-src': ['self'],
-						'connect-src': [
-							'self',
-							'api.codeball.ai',
-							'umami.codeball.ai',
-							'app.posthog.com',
-							'vitals.vercel-insights.com'
-						],
-						'img-src': ['self', 'api.producthunt.com'],
-						'child-src': ['self', 'ghbtns.com'],
-						'script-src': ['self', 'umami.codeball.ai', 'app.posthog.com']
-					}
-			  }
-			: undefined
+		}
 	}
 };
 
