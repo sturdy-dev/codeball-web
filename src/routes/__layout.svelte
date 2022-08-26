71cbf51c927cc87f4b3d4f8b53667d59f0ce05d7
Gustav Westling <gustav@getsturdy.com>
2022-08-26 16:08:22 +0200

    index: redesigned buttons

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -78,7 +78,7 @@
 	</a>
 {/if}
 
-<main class="min-h-screen bg-white md:mx-auto md:max-w-7xl mb-16">
+<main class="mb-16 min-h-screen bg-white md:mx-auto md:max-w-7xl">
 	<Header {login} />
 	<slot />
 </main>
