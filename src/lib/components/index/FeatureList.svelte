5305fb9cf804d55a1c048ca309b3240204cdf77e
Gustav Westling <gustav@getsturdy.com>
2022-08-29 13:17:21 +0200

    dora: redesigned page

---
+++ src/lib/components/index/FeatureList.svelte
@@ -0,0 +1,7 @@
+<div class="flex  w-full flex-col items-center bg-white">
+	<div
+		class="flex w-full max-w-7xl flex-col items-center space-y-4 py-16 px-4 md:space-y-16 md:px-8"
+	>
+		<slot />
+	</div>
+</div>
---
+++ src/lib/components/index/Hero.svelte
@@ -0,0 +1,13 @@
+<div class="flex w-full flex-col items-center ">
+	<div
+		class="flex w-full  max-w-7xl flex-col items-center justify-between md:flex-row md:items-start md:py-16"
+	>
+		<div class=" flex w-full flex-col  gap-8 px-4">
+			<slot name="left" />
+		</div>
+
+		<div class=" flex flex-col items-center px-4 md:max-w-[30rem]">
+			<slot name="right" />
+		</div>
+	</div>
+</div>
