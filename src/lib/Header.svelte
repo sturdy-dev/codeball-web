eb7b4b088c5ceea0886a82cb78524e292847c9d0
Gustav Westling <gustav@getsturdy.com>
2022-08-29 14:40:27 +0200

    header: mobile friendly with popover

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 
+	import {
+		Popover,
+		PopoverButton,
+		PopoverPanel,
+		PopoverOverlay
+	} from '@rgossiaux/svelte-headlessui';
+
 	export let login: string | null;
 
 	const header = [
@@ -12,37 +19,68 @@
 	];
 </script>
 
-<div class="p-4 md:mx-auto md:max-w-7xl">
-	<header
-		class="flex flex-col items-center gap-4 border-b-2 border-gray-400 pb-4 text-black md:mb-0 md:h-12 md:flex-row md:border-b-0"
-	>
-		<div class="flex h-full items-center gap-4 p-2 md:mr-12 md:p-0">
-			<a class="block h-8 text-2xl md:h-4/5" href="/"
-				><img class="h-full" src="/brand/ball/BallWithBrainRaster256.png" /></a
+<div class="md:mx-auto md:max-w-7xl">
+	<Popover class="" let:open>
+		<header class="flex items-center justify-between gap-4 p-4 text-black md:flex-row">
+			<div class="float-left flex h-full items-center gap-4 p-2 md:mr-12 md:p-0">
+				<a class="block h-8 text-2xl md:h-12" href="/"
+					><img class="h-full" src="/brand/ball/BallWithBrainRaster256.png" /></a
+				>
+				<a class="text-2xl" href="/">Codeball</a>
+			</div>
+
+			<div class="grid hidden flex-1 grid-cols-2	gap-x-4	 md:block md:flex md:space-x-2 ">
+				{#each header as { href, title }}
+					<a
+						class:text-red-700={$page.url.pathname === href}
+						class:font-semibold={$page.url.pathname === href}
+						class="whitespace-nowrap"
+						{href}>{title}</a
+					>
+				{/each}
+
+				<div class="flex-1" />
+
+				{#if login}
+					<a
+						class:text-red-700={$page.url.pathname === '/dashboard'}
+						class:font-semibold={$page.url.pathname === '/dashboard'}
+						href="/dashboard">Dashboard</a
+					>
+				{:else}
+					<a href={`/github/oauth`}>Login</a>
+				{/if}
+			</div>
+
+			<PopoverButton
+				class="inline-flex items-center justify-center rounded-md p-2 text-2xl text-gray-400 md:hidden"
 			>
-			<a class="text-2xl" href="/">Codeball</a>
-		</div>
-		<div class="grid flex-1	grid-cols-2	 gap-x-4 md:flex md:space-x-2 ">
-			{#each header as { href, title }}
-				<a
-					class:text-red-700={$page.url.pathname === href}
-					class:font-semibold={$page.url.pathname === href}
-					class="whitespace-nowrap"
-					{href}>{title}</a
-				>
-			{/each}
+				{open ? '✖️' : '🍔'}
+			</PopoverButton>
+		</header>
 
-			<div class="hidden flex-1 md:block" />
+		<PopoverPanel class="box-shadow-lg absolute top-12 z-10 w-full transform p-4 md:hidden">
+			<div class="rounded-xl bg-black p-4 pb-32 text-white drop-shadow-xl">
+				<div class="grid grid-cols-2 gap-4">
+					{#each header as { href, title }}
+						<PopoverButton
+							as="a"
+							class="whitespace-nowrap rounded-md text-base font-medium hover:text-gray-50"
+							{href}>{title}</PopoverButton
+						>
+					{/each}
 
-			{#if login}
-				<a
-					class:text-red-700={$page.url.pathname === '/dashboard'}
-					class:font-semibold={$page.url.pathname === '/dashboard'}
-					href="/dashboard">Dashboard</a
-				>
-			{:else}
-				<a href={`/github/oauth`}>Login</a>
-			{/if}
-		</div>
-	</header>
+					{#if login}
+						<PopoverButton
+							as="a"
+							class="rounded-md text-base font-medium hover:text-gray-50"
+							href="/dashboard">Dashboard</PopoverButton
+						>
+					{:else}
+						<PopoverButton as="a" href={`/github/oauth`}>Login</PopoverButton>
+					{/if}
+				</div>
+			</div>
+		</PopoverPanel>
+	</Popover>
 </div>
