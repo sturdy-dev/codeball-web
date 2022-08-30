3a02cd1e3ac5bd1eccc3aab73b709d54989224c1
Gustav Westling <gustav@getsturdy.com>
2022-08-30 13:45:26 +0200

    approver/demo: add auth step before demo is visible

---
+++ src/lib/components/auth/AuthButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import gitHubLogo from '$lib/assets/GitHub-Mark-120px-plus.png?preset=logo';
+	import Image from '$lib/components/Image.svelte';
+	import Button from '$lib/Button.svelte';
+	import { page } from '$app/stores';
+</script>
+
+<Button href={`/github/oauth?redirect=${encodeURIComponent($page.url.toString())}`} color="white">
+	<span>Login with GitHub</span>
+	<Image class="ml-1" src={gitHubLogo} alt="GitHub logo" />
+</Button>
