b6eb6cbcc9f0f0ff120b0324565f7a0781efb477
Gustav Westling <gustav@getsturdy.com>
2022-09-12 17:10:30 +0200

    svelte-migrate: narrow and wide layouts

--- src/routes/blog/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import ReadMore from '$lib/blog/ReadMore.svelte';
-</script>
-
-<article class="font-mono">
-	<h1 class="text-6xl font-black tracking-tight">Blog</h1>
-	<ReadMore title="Latest posts" />
-</article>
