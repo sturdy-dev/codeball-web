f1924877c917c4c36544f09ec2777e99af3d46dc
Gustav Westling <gustav@getsturdy.com>
2022-09-13 10:04:10 +0200

    svelte-migrate: fix dashboard pages

--- src/routes/(narrow)/dashboard/+page.svelte
+++ src/routes/(narrow)/dashboard/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-	throw new Error("@migration task: Add data prop (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292707)");
-
-	import { Dashboard } from '../../../lib/components/dashboard';
-	export let login: string | null;
+    import { Dashboard } from '../../../lib/components/dashboard';
+    export let data;
 </script>
 
-<Dashboard {login} />
+<Dashboard login={data.login} />
