6379367362331dfa31dbc11c829b3673389c4068
Gustav Westling <gustav@getsturdy.com>
2022-09-13 11:42:38 +0200

    svelte-migrate: fix index page meta tags

--- src/routes/(wide)/+page.ts
+++ src/routes/(wide)/+page.ts
@@ -1,9 +1,14 @@
-export const load = async () => ({
-	// props: { login: stuff?.login ?? null },
-	// stuff: {
-	// 	title: 'Codeball – AI Powered Code Review',
-	// 	description:
-	// 		'Codeball finds bugs in your Pull Requests, lets you ship faster and with higher confidence.'
-	// }
-	title: "YOLO"
-});
+import type {PageLoad} from "@sveltejs/kit";
+
+export const load : PageLoad= async ({parent}) => {
+	const parentData = await parent();
+
+	return {
+		...parentData,
+		meta: {
+			...parentData.meta,
+			title: 'Codeball – AI Powered Code Review',
+			description: 'Codeball finds bugs in your Pull Requests, lets you ship faster and with higher confidence.'
+		}
+	};
+};
