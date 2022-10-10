ba5e02382d686744da1da5c2ab3a8498a399c39b
Gustav Westling <gustav@getsturdy.com>
2022-10-10 09:16:29 +0200

    all: remove github outage banner

--- src/routes/(narrow)/+layout.svelte
+++ src/routes/(narrow)/+layout.svelte
@@ -11,10 +11,10 @@
 		<Header login={data.login} />
 	</div>
 
-	<div class="w-full max-w-7xl flex-1 p-4">
+	<!-- <div class="w-full max-w-7xl flex-1 p-4">
 		<div class="bg-orange-50 border-orange-200 border-2 rounded-xl py-4 px-8 text-orange-800 mb-8">🟡 We're currently experiancing issues with the GitHub API. Some features might not work as expected. Contact support@codeball.ai for assistance.</div>
 		<slot />
-	</div>
+	</div> -->
 
 	<div class="w-full bg-orange-50">
 		<Footer />
